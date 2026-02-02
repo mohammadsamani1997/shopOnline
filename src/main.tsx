be6fa3260496e893d6c3b299b2d0f9212cb46026
mohammadsamani1997 <3mohammadsamani1997@gmail.com>
2026-02-02 23:18:47 +0330

    Add floating WhatsApp widget (site-wide) with styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import GalleryPage from './pages/GalleryPage';
-import WhyUsPage from './pages/WhyUsPage';
-import ServicesPage from './pages/ServicesPage';
-import ContactPage from './pages/ContactPage';
-import './index.css';
+﻿import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import GalleryPage from "./pages/GalleryPage";
+import WhyUsPage from "./pages/WhyUsPage";
+import ServicesPage from "./pages/ServicesPage";
+import ContactPage from "./pages/ContactPage";
+import WhatsAppWidget from "./Components/WhatsAppWidget";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
@@ -19,5 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </BrowserRouter>
+
+    <WhatsAppWidget />
   </React.StrictMode>
 );
---
+++ src/main.tsx.bak
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import GalleryPage from './pages/GalleryPage';
+import WhyUsPage from './pages/WhyUsPage';
+import ServicesPage from './pages/ServicesPage';
+import ContactPage from './pages/ContactPage';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+        <Route path="/why-us" element={<WhyUsPage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
